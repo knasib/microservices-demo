1b3fe58b192b3bde2186a864ce11b2a74db40d23
nasib.sahu <nasib.sahu@asg.com>
2021-12-05 17:29:30 +0530

    Initial Project setup

---
+++ twitter-to-kafka-service/src/main/java/com/microservice/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -0,0 +1,18 @@
+package com.microservice.demo.twitter.to.kafka.service;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterToKafkaServiceApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Running..");
+    }
+}
